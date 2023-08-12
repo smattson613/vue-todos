617333e64b266064c9eccb7f3db732621492b07d
Stephen Mattson <smattson613@gmail.com>
2023-08-11 22:13:54 -0400

    first commit

---
+++ src/views/TodosView.vue
@@ -0,0 +1,83 @@
+<template>
+  <main>
+    <h1>Create Todo</h1>
+    <TodoCreator @create-todo="createTodo" />
+    <ul class="todo-list">
+      <TodoItem
+        v-for="(todo, index) in todoList"
+        :todo="todo"
+        :index="index"
+        @toggle-complete="toggleTodoComplete"
+        @edit-todo="toggleEditTodo"
+        @update-todo="updateTodo"
+      />
+    </ul>
+    <p class="todos-msg" v-if="todoList.length < 1">
+      <Icon icon="noto-v1:sad-but-relieved-face" />
+      There are no todos
+    </p>
+  </main>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { uid } from "uid";
+import { Icon } from "@iconify/vue";
+import TodoCreator from "../components/TodoCreator.vue";
+import TodoItem from "../components/TodoItem.vue";
+
+const todoList = ref([]);
+
+const createTodo = (todo) => {
+  todoList.value.push({
+    id: uid(),
+    todo,
+    isCompleted: null,
+    isEditing: false,
+  });
+};
+
+const toggleTodoComplete = (todoPosition) => {
+  todoList.value[todoPosition].isCompleted = !todoList.value[todoPosition].isCompleted;
+}
+
+const toggleEditTodo = (todoPosition) => {
+  todoList.value[todoPosition].isEditing = !todoList.value[todoPosition].isEditing;
+}
+
+const updateTodo = (todoValue, todoPostion) => {
+  todoList.value[todoPostion].todo = todoValue;
+}
+</script>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
+  }
+}
+</style>
